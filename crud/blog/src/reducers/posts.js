a70da0db1caecf938f52d8c1f62dcdf511590335
Foyzul Karim <foyzulkarim@gmail.com>
2020-04-08 23:05:04 +0800

    redux saga added

--- crud/blog/src/reducers/posts.js
+++ crud/blog/src/reducers/posts.js
@@ -4,6 +4,7 @@ export default (state = [{_id: '1', title: 'my amazing title - najim'}], action)
 
     switch (action.type) {
         case 'FETCH_POSTS_SUCCESS':
+            console.log('i am in FETCH_POSTS_SUCCESS', action);
             return action.payload;
         default:
             return state;    
--- crud/blog/src/sagas/sagas.js
+++ crud/blog/src/sagas/sagas.js
@@ -3,18 +3,7 @@ import { call, put, takeEvery, all } from "redux-saga/effects";
 export function* fetchPosts() {
     console.log('i am in fetchPosts');
     try {
-        const posts = yield call(
-            fetch('http://localhost:3001/posts/search', {
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                referrerPolicy: 'no-referrer', // no-referrer, *client
-            })
-        );
-        yield put({ type: 'FETCH_POSTS_SUCCESS', posts });
+        yield put({ type: 'FETCH_POSTS_SUCCESS', payload: [] });
     } catch (error) {
         console.log('fetch posts error', error);
     }
@@ -26,5 +15,5 @@ function* watchFetchPosts() {
 
 export default function* rootSaga() {
     console.log('i m in rootSaga');
-    yield all([watchFetchPosts]);
+    yield all([watchFetchPosts()]);
 };
